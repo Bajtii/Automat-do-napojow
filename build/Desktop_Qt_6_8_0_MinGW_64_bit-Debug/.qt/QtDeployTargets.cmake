cf09384c789d0ab5aa387b48ff39e147f2cde24d
Korius668 <korius668@gmail.com>
2024-06-04 12:37:30 +0200

    Qt UI added

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Automat-do-napojow_FILE C:/Users/koriu/Desktop/Automat-do-napojow/Automat-do-napojow/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/Automat-do-napojow.exe)
+set(__QT_DEPLOY_TARGET_Automat-do-napojow_RUNTIME_DLLS D:/PROGRAMIKI/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/PROGRAMIKI/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/PROGRAMIKI/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
